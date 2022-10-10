efc7d8aba79e19adc366b677a5be65a3cf5103cc
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-10 16:34:30 +0000

    Added file to handle exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	real_numbers = 0
-	for i in range(x):
-		try:
-			print("{}".format(my_list[i]), end= "")
-			real_numbers += 1
+    real_numbers = 0
 
-		except:
-			pass
-
-	print()
-	return real_numbers
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            real_numbers += 1
+        except:
+            pass
+    print()
+    return real_numbers
