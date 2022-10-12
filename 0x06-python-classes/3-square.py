9671b9c7b4a2788319d903da122bf46d99daa199
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-12 01:44:32 +0000

    Added public instance method to compute area of a square with privatae attributes, it works but the end user cant see the private attributes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -20,9 +20,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
     def area(self):
-        """Calculates the area of a square.
+        """Calculates the area of a square
 
         Returns:
             Area of the square
