acd0f0b086dfd573784b609ffa54789b972d7f00
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-17 17:16:57 +0000

    Fixed private attribute error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     @property
     def width(self):
         """Retrieves the value of the private instance variable width"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -38,12 +38,12 @@ class Rectangle:
 
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """REtrieves value of the private instance variable height"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -58,4 +58,4 @@ class Rectangle:
 
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
