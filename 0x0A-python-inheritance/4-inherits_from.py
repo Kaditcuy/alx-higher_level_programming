68fd62185c74c44d365f040a10d964f080004c34
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-24 22:31:59 +0000

    hot fix

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function to check is an object is an instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is the instance of an inherited class(directly or indirectly).
+
+    Args:
+        obj: instnace of class to check
+        a_class: class to check
+    Return:
+        True if its an instnace else False
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
