1563569baef6b01c6bccf2e634415db5c52d8075
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-24 18:27:25 +0000

    Added function to return all the attributes of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Function to return a list of
+        an objects attributes(i.e methods
+        and fields)
+
+    Args:
+        obj: object to lookup
+
+    Return:
+        list of the attributes of the object
+    """
+    return dir(obj);
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,6 @@
+# 0x0A. Python - Inheritance
+
+## `Python` `OOP` `Inheritance`
+
+### 0. Lookup
+* Wrote a function that returns all the list of available attributes and methods of an object
