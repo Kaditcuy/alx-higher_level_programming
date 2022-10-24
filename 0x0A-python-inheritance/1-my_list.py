8458b97895f4d940ea89cd5539bccdb2f178fe0e
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-24 20:08:36 +0000

    Added function to print a list in a sorted orde

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module containing subclass Mylist
+of parent class list
+"""
+
+
+class MyList(list):
+    """Sub-class of parent class list"""
+
+    def print_sorted(self):
+        """Functiom to print the list"""
+        print(sorted(self))
