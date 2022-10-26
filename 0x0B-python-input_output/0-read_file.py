0563571c0040fb7b528ded2d4df7b606ec76100f
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-26 13:01:18 +0000

    Added function that reads a from a textfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing function that reads a
+text file with encoding (UTF-8)
+"""
+
+def read_file(filename=""):
+    """Function to read a text file and
+    print it to stdout"""
+
+    with open(filename, 'r', encoding='utf-8') as a_file:
+        print(a_file.read())
