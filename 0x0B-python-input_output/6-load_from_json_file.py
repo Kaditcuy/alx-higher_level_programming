f87bc593c4efa3ef31f892aae101724cc7eb320b
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-26 21:23:51 +0000

    Added unction to decode json string format inside a file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module containing function that decodes JSON string format
+from a file into python objects
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Function to decode JSON string format from a file
+    into python object.
+
+    Args:
+        filename(str): name of file to decode JSON string
+        format from.
+    """
+    with open(filename, 'r+', encoding='utf-8') as a_file:
+        return json.load(a_file)
