824829b2e2a49acebed95d5610fc927e8e9f27eb
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-30 15:52:14 +0000

    Added public method to compute area of rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,78 +1,109 @@
 #!/usr/bin/python3
-"""Module containing Rectangle class
-which is a sub class of parent Base class"""
+""" Module: rectangle """
 from models.base import Base
 
+
 class Rectangle(Base):
-    """Base class, blueprint for all Base objects"""
+    """ Rectangle inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes object of the Rectangle class"""
+        """ Instantiation of instance attributes"""
+
         super().__init__(id)
         self.width = width
-        self.heigth = height
+        self.height = height
         self.x = x
         self.y = y
 
     @property
     def width(self):
-        """getter method for private
-        instance variable width"""
+        """
+            getter function for __width
+            Returns: width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter method for private
-        instance variable width"""
-        if type(value) is not int:
+        """
+            setter function for width.
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
     def height(self):
-        """getter method for private
-        instance variable height"""
+        """
+            getter function for height
+            Returns: height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter method for private
-        instance variable height"""
+        """
+            setter function for height
+            Args:
+                value (int): value to be set.
+        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
     def x(self):
-        """getter method for private
-        instance variable x"""
+        """
+            getter function for x.
+            Returns: x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """setter method for private imstance
-        variable x"""
+        """
+            setter function for x.
+            Args:
+                value (int): value to be set.
+        """
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
     def y(self):
-        """getter method for private
-        instance variable y"""
+        """
+            getter function for y
+            Returns: y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """setter method for private
-        instance variable y"""
+        """
+            setter function for y
+            Args:
+                value (int): value to be set.
+        """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
+
+        self.__y = value
+
+    def area(self):
+        """Computes area of a reactangle"""
+
+        return self.__width * self.__height
