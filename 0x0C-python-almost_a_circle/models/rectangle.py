85fb529f2c644c0882cf4e9192419c855dbfe553
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-30 15:36:37 +0000

    Added validation to setting of priave instance variables

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be a positive integer")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -43,7 +43,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be a positive integer")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -59,7 +59,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be a positive integer")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -75,4 +75,4 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be a positive integer")
+            raise ValueError("y must be >= 0")
