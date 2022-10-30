659a7f3660d866643f25290ed954190aa7a018fb
Kaditcuy <philipukchimaobi@gmail.com>
2022-10-30 20:27:28 +0000

    Added public mthod that uses kwargs if no args are passed to function to set the attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,7 +130,7 @@ class Rectangle(Base):
         return  "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y
                 , self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
             updates the attributes
             with each arguments passed to 
@@ -149,3 +149,15 @@ class Rectangle(Base):
                 self.__x = args[3]
             if len(args) > 4:
                 self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
