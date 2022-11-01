232f437b061a175513f02485a8bcc2edb2cc53dc
Kaditcuy <philipukchimaobi@gmail.com>
2022-11-01 15:13:58 +0000

    Added method that returns the dictionary representaion of all the attributes of the object

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -161,3 +161,14 @@ class Rectangle(Base):
                     self.__x = value
                 if key == "y":
                     self.__y = value
+
+    def to_dictionary(self):
+        """returns dictionary representation
+        of rectangle"""
+        return {
+            "id": self.id,
+            "width":self.__width,
+            "height":self.__height,
+            "x":self.__x,
+            "y":self.__y
+        }
