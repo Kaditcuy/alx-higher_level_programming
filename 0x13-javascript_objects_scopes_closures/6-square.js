c9e88db7f057d0662c3d26c3b8cb510818e0a6b7
Kaditcuy <philipukchimaobi@gmail.com>
2022-12-22 03:20:07 +0000

    File upload from 1 - 101

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Base = require('./5-square.js');
+
+module.exports = class Square extends Base {
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
+Footer
+© 2022 GitHub, Inc.
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,3 @@
+# 0x13. JavaScript - Objects, Scopes and Closures
+
+* Object oriented programming in javascript
