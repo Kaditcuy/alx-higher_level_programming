e702644dce5c6d6620fcbd79c8f75a1d7242fb5f
Kaditcuy <philipukchimaobi@gmail.com>
2023-01-18 04:43:17 +0000

    hot fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Module that uses request package to fetch url's"""
+"""
+Module that uses request package to fetch url's
+"""
+import requests
 
 if __name__ == "__main__":
-    import requests
 
     url = 'https://alx-intranet.hbtn.io/status'
     res = requests.get(url)
