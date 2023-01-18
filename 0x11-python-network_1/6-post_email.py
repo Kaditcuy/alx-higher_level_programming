3a0face76f48380dc89fe3c71424d897b40233be
Kaditcuy <philipukchimaobi@gmail.com>
2023-01-18 04:40:03 +0000

    hot fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Module containing python script that posts a request to a server"""
+"""
+Module containing python script that posts a request to a server
+"""
+import requests
+from sys import argv
 
 if __name__ = "__main__":
-    import requests
-    from sys import argv
-
     email = {'email': argv[2]}
     url = argv[1]
     res = requests.post(url, data=email)
