c8d8c698d2721714e067c15a83e3fe649dbb86d4
Kaditcuy <philipukchimaobi@gmail.com>
2023-01-18 04:25:00 +0000

    python script to post to a url

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin.python3
+"""Module that takes in a letter and sends a POST request to a
+url with the letter as parameter"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    q = argv[1] if len(argv[1]) > 1 else ''
+    letter = {'q': q}
+    res = requests.post('http://0.0.0.0:5000/search_user', data=letter)
+    try:
+        output = res.json()
+        if output == {}:
+            print('No result')
+        else:
+            print('[{}] {}'.format(output.get('id'), output.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
