5e8f1fd1885b004219c2d0afc59764eb03b7186d
Kaditcuy <philipukchimaobi@gmail.com>
2023-01-18 04:35:48 +0000

    hot fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,11 +6,10 @@ displays body of the response
 if error occurs, prints 'Error code:' followed by HTTP status code
 Usage: ./7-error_code.py <url>
 '''
+from sys import argv
+import requests
 
 if __name__ == '__main__':
-	import requests
-	from sys import argv
-
     res = requests.get(argv[1])
     if res.status_code >= 400:
         print('Error code: {}'.format(res.status_code))
