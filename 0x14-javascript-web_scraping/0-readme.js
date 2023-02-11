4fcc5a2bc8cd1062109dbb566cde54224f26b6e7
Kaditcuy <philipukchimaobi@gmail.com>
2023-02-11 03:14:15 +0000

    Added more node scripts to fetch internet resources

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (error, data) {
-	if (!error) {
-		console.log(data.toString());
-	} else {
-		console.log(error);
-	}
+  if (!error) {
+    console.log(data.toString());
+  } else {
+    console.log(error);
+  }
 });
